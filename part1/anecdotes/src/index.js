f240c01ee6a819ed05b6bbd6554072bfe3b9cafd
Ismael Navarro <ismanapa@gmail.com>
2019-07-11 01:40:42 +0200

    Exercise 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,25 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const App = (props) => {
-    const [selected, setSelected] = useState(0);
-
-    const setNextAnecdote = () => {
-        setSelected(getRandomInt(0, anecdotes.length - 1));
-    };
-
-    return (
-        <div>
-            {props.anecdotes[selected]}
-            <div>
-                <button onClick={setNextAnecdote}>next anecdote</button>
-            </div>
-        </div>
-    );
-}
-
 const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -28,6 +9,35 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+    const setNextAnecdote = () => {
+        setSelected(getRandomInt(0, anecdotes.length - 1));
+    };
+
+    const voteCurrentAnecdote = () => {
+        votes[selected] += 1;
+        setVotes([...votes]);
+    };
+
+    return (
+        <div>
+            <h3>{props.anecdotes[selected]}</h3>
+            <p>has {votes[selected]} votes</p>
+            <div>
+                <button onClick={voteCurrentAnecdote}>vote</button>
+                <button onClick={setNextAnecdote}>next anecdote</button>
+            </div>
+        </div>
+    );
+}
+
+
 
 ReactDOM.render(
     <App anecdotes={anecdotes} />,
