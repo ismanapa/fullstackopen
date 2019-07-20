e0c751bab77cd53d5a1fa5aa5cd7af70bd310017
Ismael Navarro <ismanapa@gmail.com>
2019-07-20 17:57:08 +0200

    Deleted unused files

--- part4/phonebook/src/components/filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Filter = ({ handleFilterChange, filter }) => (
-	<div>
-        filter shown with <input onChange={handleFilterChange} value={filter} />
-	</div>
-);
-
-export default Filter;
--- part4/phonebook/src/components/persons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Person from './person';
-
-const Persons = ({ persons, onDelete }) => (
-	persons.map(p => <Person onDelete={onDelete} key={p.name} person={p} />)
-);
-
-export default Persons;
