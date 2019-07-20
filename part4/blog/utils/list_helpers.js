8428255b0c5f724c4eb9aeba620f4a4162e4c073
Ismael Navarro <ismanapa@gmail.com>
2019-07-20 19:29:21 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helpers.js
+++ part4/blog/utils/list_helpers.js
@@ -6,7 +6,19 @@ const totalLikes = blogs => {
 	return blogs.reduce((ac, blog) => ac + blog.likes, 0);
 };
 
+const favoriteBlog = blogs => {
+	if (favoriteBlog.length === 0)
+		return undefined;
+
+	const sortedBlogs = [...blogs].sort((a, b) => {
+		return b.likes - a.likes;
+	});
+
+	return sortedBlogs[0];
+};
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };
--- part4/blog/utils/list_helpers.spec.js
+++ part4/blog/utils/list_helpers.spec.js
@@ -92,3 +92,66 @@ describe('totalLikes', () => {
 	});
 
 });
+
+describe('favoriteBlog', () => {
+
+	test('return blog most liked', () => {
+		const blogs = [
+			{
+				_id: '5a422a851b54a676234d17f7',
+				title: 'React patterns',
+				author: 'Michael Chan',
+				url: 'https://reactpatterns.com/',
+				likes: 7,
+				__v: 0
+			},
+			{
+				_id: '5a422aa71b54a676234d17f8',
+				title: 'Go To Statement Considered Harmful',
+				author: 'Edsger W. Dijkstra',
+				url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+				likes: 5,
+				__v: 0
+			},
+			{
+				_id: '5a422b3a1b54a676234d17f9',
+				title: 'Canonical string reduction',
+				author: 'Edsger W. Dijkstra',
+				url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+				likes: 12,
+				__v: 0
+			},
+		];
+
+		const result = listHelper.favoriteBlog(blogs);
+
+		expect(result).toEqual(blogs[2]);
+	});
+
+	test('return blog most liked when there is only one blog', () => {
+		const blogs = [
+			{
+				_id: '5a422a851b54a676234d17f7',
+				title: 'React patterns',
+				author: 'Michael Chan',
+				url: 'https://reactpatterns.com/',
+				likes: 7,
+				__v: 0
+			},
+		];
+
+		const result = listHelper.favoriteBlog(blogs);
+
+		expect(result).toEqual(blogs[0]);
+	});
+
+
+	test('return undefined when there is no blogs', () => {
+		const blogs = [];
+
+		const result = listHelper.favoriteBlog(blogs);
+
+		expect(result).toEqual(undefined);
+	});
+
+});
