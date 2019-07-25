0c0ee6a137bb1cb8ecfed211b3e1b4eb787f013c
Ismael Navarro <ismanapa@gmail.com>
2019-07-26 00:24:41 +0200

    4.8: Blog list tests, step1

---
+++ part4/blog/tests/blog_api.spec.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+
+const initialBlogs = [
+	{
+		title: 'test blog 1',
+		author: 'test author 1',
+		url: 'test url 1',
+		likes: 100,
+	},
+	{
+		title: 'test blog 2',
+		author: 'test author 2',
+		url: 'test url 2',
+		likes: 200,
+	},
+	{
+		title: 'test blog 3',
+		author: 'test author 3',
+		url: 'test url 3',
+		likes: 300,
+	}
+];
+
+describe('blog api', () => {
+
+	beforeEach(async () => {
+		await Blog.deleteMany({});
+
+		const blogObjects = initialBlogs
+			.map(blog => new Blog(blog))
+		const promiseArray = blogObjects.map(blog => blog.save());
+
+		await Promise.all(promiseArray);
+
+	});
+
+	test('Blogs are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+	});
+
+	test('All blogs are returned', async () => {
+		const response = await api.get('/api/blogs');
+
+		expect(response.body.length).toBe(initialBlogs.length);
+	});
+
+	afterAll(() => {
+		mongoose.connection.close();
+	});
+
+});
