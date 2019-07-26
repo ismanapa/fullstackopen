4196b5a603d5218211ca0db6306d2bfc976e3057
Ismael Navarro <ismanapa@gmail.com>
2019-07-26 18:02:48 +0200

    4.20*: bloglist expansion, step9

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,13 +3,6 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization');
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.substring(7);
-	}
-	return null;
-};
 
 blogsRouter.get('/', async (request, response, next) => {
 	try {
@@ -22,11 +15,10 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
 	try {
-		const token = getTokenFrom(request);
 
-		const decodedToken = jwt.verify(token, process.env.SECRET);
+		const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
-		if (!token || !decodedToken.id) {
+		if (!request.token || !decodedToken.id) {
 			return response.status(401).json({ error: 'token missing or invalid' });
 		}
 
