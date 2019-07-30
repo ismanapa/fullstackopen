d01654e64b8aea05fe19eb5aa41d662b18c4f906
Ismael Navarro <ismanapa@gmail.com>
2019-07-31 00:35:29 +0200

    4.14: Blog list tests, step2

--- part5/blog-frontend/src/components/simpleBlog.spec.js
+++ part5/blog-frontend/src/components/simpleBlog.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent  } from '@testing-library/react';
 import SimpleBlog from './SimpleBlog';
 
 afterEach(cleanup);
@@ -19,6 +19,23 @@ describe('SimpleBlog', () => {
 		const header = component.container.querySelector('.header');
 		expect(header).toHaveTextContent(blog.title);
 		expect(header).toHaveTextContent(blog.author);
+	});
+
+	test('like handler is invoken when button is pressed', () => {
+		const blog = {
+			title: 'test title',
+			author: 'test author',
+			likes: 100
+		};
+		const likeHandler = jest.fn();
+
+		const component = render(<SimpleBlog blog={blog} onClick={likeHandler}/>);
+		
+		const button = component.container.querySelector('.likeButton');
+		fireEvent.click(button);
+		fireEvent.click(button);
+
+		expect(likeHandler).toBeCalledTimes(2);
 	});
 
 });
