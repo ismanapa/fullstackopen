e8de036b6d17485029302230510182d94023c4ca
Ismael Navarro <ismanapa@gmail.com>
2019-07-30 23:59:36 +0200

    5.8*: Blog list frontend, step8

--- part5/blog-frontend/src/App.js
+++ part5/blog-frontend/src/App.js
@@ -156,8 +156,10 @@ const App = () => {
 							</Toggable>
 
 							<div style={{ marginTop: 20 }}>
-								{blogs && blogs.map(blog =>
-									<Blog key={blog.id} blog={blog} handleLike={likeBlog} />)
+								{blogs && blogs
+									.sort((a, b) => b.likes - a.likes)
+									.map(blog =>
+										<Blog key={blog.id} blog={blog} handleLike={likeBlog} />)
 								}
 							</div>
 
