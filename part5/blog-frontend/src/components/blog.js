89a2d69b07c8f199788cfed780f1db63f2ac6f08
Ismael Navarro <ismanapa@gmail.com>
2019-07-30 23:47:45 +0200

    5.6* Blog list frontend, step6

--- part5/blog-frontend/src/components/blog.js
+++ part5/blog-frontend/src/components/blog.js
@@ -1,8 +1,35 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+	const [visible, setVisible] = useState(false);
+
+	const blogStyle = {
+		padding: 10,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5,
+		borderRadius: 2
+	};
+
+
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
+
+	return (
+		<div style={blogStyle}>
+			<div onClick={toggleVisibility}>
+				{blog.title} - {blog.author}
+			</div>
+
+			<div style={{ display: visible  ? 'block' : 'none'}} >
+				<hr/>
+				<div>url: {blog.url}</div>
+				<div>likes: {blog.likes} <button>like</button> </div>
+				<div>added by {blog.author}</div>
+			</div>
+		</div>
+	);
+};
 
 export default Blog;
